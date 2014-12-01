6f9b42784c6050dfadb82e6ce28ac825aef5d1c1
Gaurav Ramanan <gaurav@rcorp.co.in>
2014-12-01 16:47:28 +0530

    Add: First Commit

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-bhasha
-======
+#BHA-SHA
+*Readable hashes in Human Languages!*
 
-Readable hashes in Human Languages
+BHA-SHA is a [recursive acronym](http://en.wikipedia.org/wiki/Recursive_acronym) that stands for **B**hasha **H**ashing **A**lgorithms as **S**ecure **H**ashing **A**lgorithms. 
+
+## Getting Started
+    npm install -g bhasha
+
+    var bhasha = require('bhasha');
+    console.log(bhasha.simpleSentence('Hello World'));
+
+## Advanced Usage
+TODO
+
+###Options
+<table>
+  <tr>
+    <td>Hash Function</td>
+    <td><i>String</i></td>
+    <td><code>'sha1', 'md5', 'sha512'</code></td>
+  </tr>
+  <tr>
+	  <td>
+		  TODO
+      </td>
+  </tr>
+</table>
+
+## How to Contribute
+### Reporting a bug
+Coming Soon
+
+### Suggesting a Feature
+Coming Soon
+
+### Submitting patches
+Coming Soon
+
+## License
+Code and documentation copyright 2014 Ramanan Corporation. 
+Code released under the MIT license. Docs released under Creative Commons with Attribution.
---
+++ process.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+var wordPatt = /^(\S+)/mg;
+var crypto = require('crypto');
+
+var nounString = fs.readFileSync('dict/index.noun', 'utf-8');
+fs.writeFileSync('nouns.json', JSON.stringify(nounString.match(wordPatt)));
+
+var adjectiveString = fs.readFileSync('dict/index.adj', 'utf-8');
+fs.writeFileSync('adjectives.json', JSON.stringify(adjectiveString.match(wordPatt)));
+
+var verbString = fs.readFileSync('dict/index.verb', 'utf-8');
+fs.writeFileSync('verbs.json', JSON.stringify(verbString.match(wordPatt)));
+
+var adverbString = fs.readFileSync('dict/index.adv', 'utf-8');
+fs.writeFileSync('adverbs.json', JSON.stringify(adverbString.match(wordPatt)));
+
+console.log ('Nouns: ' + nounString.match(wordPatt).length)
+console.log ('Adjectives: ' + adjectiveString.match(wordPatt).length)
+console.log ('Verbs: ' + verbString.match(wordPatt).length)
+console.log ('Adverbs: ' + adverbString.match(wordPatt).length)
+
+shasum = crypto.createHash('sha1');
+shasum.update(Math.random().toString(), 'utf8')
+var sha = shasum.digest('hex');
+
+var returnObject = {
+	"simpleSentence": function (sha){
+		//Split the sha into the first 8 hexadecimal digits (ie. 8 * 4 = 32 bits) and
+		//convert to decimal system (base 10)
+		var dec = parseInt(sha.slice(0, 7), 16);
+
+		var nounIndex = Math.floor(dec / adjectiveString.match(wordPatt).length);
+		var adjectiveIndex = Math.floor (dec % adjectiveString.match(wordPatt).length);
+
+		console.log (adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex])
+		console.log (adjectiveIndex, nounIndex);
+	},
+
+	"compoundSentence": function (sha){
+		//Split the sha into the first 14 hexadecimal digits (ie. 14 * 4 = 56 bits) and
+		//convert to decimal system (base 10)
+		var dec = parseInt(sha.slice(0, 14), 16);
+		//var dec = 1678686549755813888;
+
+		var residue = Math.floor(dec / adverbString.match(wordPatt).length);
+		var adverbIndex = Math.floor (dec % adverbString.match(wordPatt).length);
+
+		residue = Math.floor(residue / verbString.match(wordPatt).length);
+		var verbIndex = Math.floor(residue % verbString.match(wordPatt).length);
+		
+		var nounIndex = Math.floor(residue / adjectiveString.match(wordPatt).length);
+		var adjectiveIndex = Math.floor(residue % adjectiveString.match(wordPatt).length);
+
+		console.log (adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex], verbString.match(wordPatt)[verbIndex], adverbString.match(wordPatt)[adverbIndex])
+		console.log (adjectiveIndex, nounIndex, verbIndex, adverbIndex);
+	}
+}
+
+
+returnObject['compoundSentence'](sha)
