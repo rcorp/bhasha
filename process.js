c32759a83c9112aacfb13af70bd78e9f3977bc8f
GauravVashist <gauravvashist92@gmail.com>
2014-12-03 12:05:42 +0530

    dynamicCryptography

--- process.js
+++ process.js
@@ -1,60 +1,88 @@
-var fs = require('fs');
-var wordPatt = /^(\S+)/mg;
-var crypto = require('crypto');
+	var fs = require('fs');
+	var wordPatt = /^(\S+)/mg;
+	var crypto = require('crypto');
 
-var nounString = fs.readFileSync('dict/index.noun', 'utf-8');
-fs.writeFileSync('nouns.json', JSON.stringify(nounString.match(wordPatt)));
 
-var adjectiveString = fs.readFileSync('dict/index.adj', 'utf-8');
-fs.writeFileSync('adjectives.json', JSON.stringify(adjectiveString.match(wordPatt)));
+	var nounString = fs.readFileSync('dict/index.noun', 'utf-8');
+	fs.writeFileSync('nouns.json', JSON.stringify(nounString.match(wordPatt)));
 
-var verbString = fs.readFileSync('dict/index.verb', 'utf-8');
-fs.writeFileSync('verbs.json', JSON.stringify(verbString.match(wordPatt)));
+	var adjectiveString = fs.readFileSync('dict/index.adj', 'utf-8');
+	fs.writeFileSync('adjectives.json', JSON.stringify(adjectiveString.match(wordPatt)));
 
-var adverbString = fs.readFileSync('dict/index.adv', 'utf-8');
-fs.writeFileSync('adverbs.json', JSON.stringify(adverbString.match(wordPatt)));
+	var verbString = fs.readFileSync('dict/index.verb', 'utf-8');
+	fs.writeFileSync('verbs.json', JSON.stringify(verbString.match(wordPatt)));
 
-console.log ('Nouns: ' + nounString.match(wordPatt).length)
-console.log ('Adjectives: ' + adjectiveString.match(wordPatt).length)
-console.log ('Verbs: ' + verbString.match(wordPatt).length)
-console.log ('Adverbs: ' + adverbString.match(wordPatt).length)
+	var adverbString = fs.readFileSync('dict/index.adv', 'utf-8');
+	fs.writeFileSync('adverbs.json', JSON.stringify(adverbString.match(wordPatt)));
 
-shasum = crypto.createHash('sha1');
-shasum.update(Math.random().toString(), 'utf8')
-var sha = shasum.digest('hex');
+	console.log('Nouns: ' + nounString.match(wordPatt).length)
+	console.log('Adjectives: ' + adjectiveString.match(wordPatt).length)
+	console.log('Verbs: ' + verbString.match(wordPatt).length)
+	console.log('Adverbs: ' + adverbString.match(wordPatt).length)
 
-var returnObject = {
-	"simpleSentence": function (sha){
-		//Split the sha into the first 8 hexadecimal digits (ie. 8 * 4 = 32 bits) and
-		//convert to decimal system (base 10)
-		var dec = parseInt(sha.slice(0, 7), 16);
 
-		var nounIndex = Math.floor(dec / adjectiveString.match(wordPatt).length);
-		var adjectiveIndex = Math.floor (dec % adjectiveString.match(wordPatt).length);
+	// options = {
+	// 	cryptotype:'md5'||'sha1' ||'sha256' ||'sha512'	,
+	// 	numberOfWords: 2
+	// }
+	module.exports = {
+		hash: function(options, inputString, language) {
+		if(options.cryptotype == '' || options.cryptotype == null || !options.cryptotype) {
+			shasum = crypto.createHash('sha1');	
+		} else {
+			shasum = crypto.createHash(options.cryptotype);
+		}
 
-		console.log (adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex])
-		console.log (adjectiveIndex, nounIndex);
-	},
+		shasum.update(Math.random().toString(), 'utf8')
+		var sha = shasum.digest('hex');
 
-	"compoundSentence": function (sha){
-		//Split the sha into the first 14 hexadecimal digits (ie. 14 * 4 = 56 bits) and
-		//convert to decimal system (base 10)
-		var dec = parseInt(sha.slice(0, 14), 16);
-		//var dec = 1678686549755813888;
+		console.log(sha)
+		if (options.numberOfWords == 2) {
+			//Split the sha into the first 8 hexadecimal digits (ie. 8 * 4 = 32 bits) and
+			//convert to decimal system (base 10)
+			var dec = parseInt(sha.slice(0, 7), 16);
 
-		var residue = Math.floor(dec / adverbString.match(wordPatt).length);
-		var adverbIndex = Math.floor (dec % adverbString.match(wordPatt).length);
+			var nounIndex = Math.floor(dec / adjectiveString.match(wordPatt).length);
+			var adjectiveIndex = Math.floor(dec % adjectiveString.match(wordPatt).length);
 
-		residue = Math.floor(residue / verbString.match(wordPatt).length);
-		var verbIndex = Math.floor(residue % verbString.match(wordPatt).length);
-		
-		var nounIndex = Math.floor(residue / adjectiveString.match(wordPatt).length);
-		var adjectiveIndex = Math.floor(residue % adjectiveString.match(wordPatt).length);
+			console.log(adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex])
+			console.log(adjectiveIndex, nounIndex);
 
-		console.log (adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex], verbString.match(wordPatt)[verbIndex], adverbString.match(wordPatt)[adverbIndex])
-		console.log (adjectiveIndex, nounIndex, verbIndex, adverbIndex);
+		} else if (options.numberOfWords == 4) {
+			//Split the sha into the first 14 hexadecimal digits (ie. 14 * 4 = 56 bits) and
+			//convert to decimal system (base 10)
+			var dec = parseInt(sha.slice(0, 14), 16);
+			//var dec = 1678686549755813888;
+
+			var residue = Math.floor(dec / adverbString.match(wordPatt).length);
+			var adverbIndex = Math.floor(dec % adverbString.match(wordPatt).length);
+
+			residue = Math.floor(residue / verbString.match(wordPatt).length);
+			var verbIndex = Math.floor(residue % verbString.match(wordPatt).length);
+
+			var nounIndex = Math.floor(residue / adjectiveString.match(wordPatt).length);
+			var adjectiveIndex = Math.floor(residue % adjectiveString.match(wordPatt).length);
+
+			console.log(adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex], verbString.match(wordPatt)[verbIndex], adverbString.match(wordPatt)[adverbIndex])
+			console.log(adjectiveIndex, nounIndex, verbIndex, adverbIndex);
+
+		} else {
+			//Split the sha into the first 14 hexadecimal digits (ie. 14 * 4 = 56 bits) and
+			//convert to decimal system (base 10)
+			var dec = parseInt(sha.slice(0, 14), 16);
+			//var dec = 1678686549755813888;
+
+			var residue = Math.floor(dec / adverbString.match(wordPatt).length);
+			var adverbIndex = Math.floor(dec % adverbString.match(wordPatt).length);
+
+			residue = Math.floor(residue / verbString.match(wordPatt).length);
+			var verbIndex = Math.floor(residue % verbString.match(wordPatt).length);
+
+			var nounIndex = Math.floor(residue / adjectiveString.match(wordPatt).length);
+			var adjectiveIndex = Math.floor(residue % adjectiveString.match(wordPatt).length);
+
+			console.log(adjectiveString.match(wordPatt)[adjectiveIndex], nounString.match(wordPatt)[nounIndex], verbString.match(wordPatt)[verbIndex], adverbString.match(wordPatt)[adverbIndex])
+			console.log(adjectiveIndex, nounIndex, verbIndex, adverbIndex);		
+		}
 	}
 }
-
-
-returnObject['compoundSentence'](sha)
