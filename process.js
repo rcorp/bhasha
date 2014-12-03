967256e31b10a1a49192e3e4026c68f0fc7381a6
GauravVashist <gauravvashist92@gmail.com>
2014-12-03 15:43:39 +0530

    modifiedForIfDataExistsGenerateSameKey

--- process.js
+++ process.js
@@ -1,40 +1,54 @@
-	var fs = require('fs');
-	var wordPatt = /^(\S+)/mg;
-	var crypto = require('crypto');
+var fs = require('fs');
+var wordPatt = /^(\S+)/mg;
+var crypto = require('crypto');
 
 
-	var nounString = fs.readFileSync('dict/index.noun', 'utf-8');
-	fs.writeFileSync('nouns.json', JSON.stringify(nounString.match(wordPatt)));
+var nounString = fs.readFileSync('dict/index.noun', 'utf-8');
+fs.writeFileSync('nouns.json', JSON.stringify(nounString.match(wordPatt)));
 
-	var adjectiveString = fs.readFileSync('dict/index.adj', 'utf-8');
-	fs.writeFileSync('adjectives.json', JSON.stringify(adjectiveString.match(wordPatt)));
+var adjectiveString = fs.readFileSync('dict/index.adj', 'utf-8');
+fs.writeFileSync('adjectives.json', JSON.stringify(adjectiveString.match(wordPatt)));
 
-	var verbString = fs.readFileSync('dict/index.verb', 'utf-8');
-	fs.writeFileSync('verbs.json', JSON.stringify(verbString.match(wordPatt)));
+var verbString = fs.readFileSync('dict/index.verb', 'utf-8');
+fs.writeFileSync('verbs.json', JSON.stringify(verbString.match(wordPatt)));
 
-	var adverbString = fs.readFileSync('dict/index.adv', 'utf-8');
-	fs.writeFileSync('adverbs.json', JSON.stringify(adverbString.match(wordPatt)));
+var adverbString = fs.readFileSync('dict/index.adv', 'utf-8');
+fs.writeFileSync('adverbs.json', JSON.stringify(adverbString.match(wordPatt)));
 
-	console.log('Nouns: ' + nounString.match(wordPatt).length)
-	console.log('Adjectives: ' + adjectiveString.match(wordPatt).length)
-	console.log('Verbs: ' + verbString.match(wordPatt).length)
-	console.log('Adverbs: ' + adverbString.match(wordPatt).length)
+console.log('Nouns: ' + nounString.match(wordPatt).length)
+console.log('Adjectives: ' + adjectiveString.match(wordPatt).length)
+console.log('Verbs: ' + verbString.match(wordPatt).length)
+console.log('Adverbs: ' + adverbString.match(wordPatt).length)
 
 
-	// options = {
-	// 	cryptotype:'md5'||'sha1' ||'sha256' ||'sha512'	,
-	// 	numberOfWords: 2
-	// }
-	module.exports = {
-		hash: function(options, inputString, language) {
+// options = {
+// 	cryptotype:'md5'||'sha1' ||'sha256' ||'sha512'	,
+// 	numberOfWords: 2
+// }
+module.exports = {
+	hash: function(options, inputString, dataArray) {
 		if(options.cryptotype == '' || options.cryptotype == null || !options.cryptotype) {
 			shasum = crypto.createHash('sha1');	
 		} else {
 			shasum = crypto.createHash(options.cryptotype);
 		}
 
-		shasum.update(Math.random().toString(), 'utf8')
-		var sha = shasum.digest('hex');
+		// console.log(inputString)
+		for(var i=0; i<=dataArray.length;dataArray++) {
+			if (dataArray[i] == inputString) {
+				console.log('here1')
+				exists = 1;
+			} else {
+				console.log('here0')
+				exists = 0;
+			}
+		}
+		if(exists == 1) {
+			sha = shasum.digest('hex');
+		} else if (exists == 0) {
+			shasum.update(Math.random().toString(), 'utf8')
+			sha = shasum.digest('hex');
+		}
 
 		console.log(sha)
 		if (options.numberOfWords == 2) {
