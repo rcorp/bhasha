51b3a4a8883c29ffd7c0e5638768ca74062a9adf
GauravVashist <gauravvashist92@gmail.com>
2014-12-03 15:41:07 +0530

    ifDataExistsGenerateSameKey

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var fs = require('fs');
-var enc = require('./newprocess.js')
+var enc = require('./process.js')
 options = {
-	cryptotype:'md5',
+	cryptotype: 'md5',
 	numberOfWords: 2,
-	language: en
+	language: "en"
 }
-var inputString = fs.ReadStream('file.txt');
-enc.hash(options, inputString)
+dataArray = [];
+fs.readFile('file.txt', 'utf8', function(err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		// dataArray.push(data)
+	}
+
+	fs.readFile('file1.txt', 'utf8', function(err, data1) {
+		if (err) {
+			console.log(err);
+		} else {
+			dataArray.push(data1)
+		}
+
+		fs.readFile('file2.txt', 'utf8', function(err, data2) {
+			if (err) {
+				console.log(err);
+			} else {
+				dataArray.push(data2)
+			}
+
+			// console.log('dataArray', dataArray)
+			enc.hash(options, data, dataArray)
+		})
+	})
+})
